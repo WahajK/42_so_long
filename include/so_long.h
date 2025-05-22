d66b3104d70a3446a80b64969403fdfd10e8d824
WahajK <wahajkhan788@gmail.com>
2025-05-22 23:15:15 +0200

    updated header file

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:48:24 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/05/17 14:52:07 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/05/22 23:08:20 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define SO_LONG_H
 
 # include <unistd.h>
-# include  "../libft/libft.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 #endif
