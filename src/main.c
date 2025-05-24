25ebbfea32a411f601fe63e14b4cd1ca1231cda3
WahajK <wahajkhan788@gmail.com>
2025-05-24 16:54:15 +0200

    Added some map checks in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:52:20 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/05/24 15:06:38 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/05/24 15:34:32 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,76 @@ int	ft_read_map(char *fname, t_map *map)
 	return (0);
 }
 
+void	ft_print_map(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (map->map[i])
+	{
+		ft_printf("%s\n", map->map[i++]);
+	}
+}
+
+int	ft_check_escape(t_map *map)
+{
+	return (0);
+}
+
+int	ft_check_rectangle(t_map *map)
+{
+	return (0);
+}
+
+int	ft_is_legal_char(char ch)
+{
+	if (ch == '1' || ch == '0' || ch == 'P' || ch == 'E' || ch == 'C')
+		return (0);
+	return (1);
+}
+
+int	ft_check_illegal_char(t_map *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map->map[i])
+	{
+		j = 0;
+		while (map->map[i][j])
+		{
+			if (ft_is_legal_char(map->map[i][j]))
+				return (1);
+		}
+	}
+	return (0);
+}
+
+int	ft_check_borders(t_map *map)
+{
+	return (0);
+}
+
+int	ft_validate_map(t_map *map)
+{
+	if (ft_check_borders(map) || ft_check_illegal_char(map)
+		|| ft_check_rectangle(map) || ft_check_escape(map)
+		|| map->row_count < 2)
+		return (1);
+	return (0);
+}
+
+int	ft_destructor(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (map->map[i])
+		free(map->map[i++]);
+	free(map->map);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_map	map;
@@ -80,10 +150,9 @@ int	main(int argc, char *argv[])
 		return (ft_printf("Invalid file extension\n"), 1);
 	if (ft_read_map(argv[1], &map))
 		return (ft_printf("Failed to open the file\n", 1));
-	int i = 0;
-	while (map.map[i])
-	{
-		ft_printf("%s\n", map.map[i++]);
-	}
+	if (ft_validate_map(&map))
+		return (ft_printf("Invalid map"), 1);
+	ft_print_map(&map);
+	ft_destructor(&map);
 	return (0);
 }
