326619d82577c2f13d9a718179db2d288223eb31
WahajK <wahajkhan788@gmail.com>
2025-05-29 20:57:51 +0200

    Made ft_strlen NULL safe

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:20:35 by muhakhan          #+#    #+#             */
-/*   Updated: 2024/12/03 20:11:29 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/05/29 19:15:31 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (!s)
+		return (-1);
 	i = 0;
 	while (s[i])
 		i++;
