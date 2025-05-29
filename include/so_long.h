3ac4532d7fe05edf5705c755aeb2affaec55f550
WahajK <wahajkhan788@gmail.com>
2025-05-29 20:58:17 +0200

    Added some vars to struct

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:48:24 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/05/24 14:57:55 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/05/29 14:43:26 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 
+# define TILE_SIZE 64
 typedef struct s_map
 {
 	char	**map;
 	int		row_count;
 	int		col_count;
+	int		player_pos[2];
+	int		c_count;
+	int		e_count;
+	int		p_count;
+	int		x_count;
 }	t_map;
 
 #endif
