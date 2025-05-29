7f4d44d7ce2fbb26bbd069cf3b9765143dd37169
WahajK <wahajkhan788@gmail.com>
2025-05-29 20:59:16 +0200

    Fixed map validators and improved code quality

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 14:52:20 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/05/25 20:11:50 by muhakhan         ###   ########.fr       */
+/*   Updated: 2025/05/29 20:55:02 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,43 @@ int	check_extension(char *fname)
 	return (0);
 }
 
-int	get_row_count(char *fname)
+int	char_count(char *line, char ch)
+{
+	int	i;
+
+	i = 0;
+	while (*line)
+		if (*line++ == ch)
+			i++;
+	return (i);
+}
+
+char	*trim(char *arr)
+{
+	if (!arr)
+		return (NULL);
+	if (arr[ft_strlen(arr) - 1] == '\n')
+		arr[ft_strlen(arr) - 1] = 0;
+	return (arr);
+}
+
+int	illegal_char(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '0' || str[i] == '1' || str[i] == 'P'
+			|| str[i] == 'E' || str[i] == 'C' || str[i] == 'X')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+int	set_counts(t_map *map, char *fname)
 {
 	int		fd;
 	int		i;
@@ -29,22 +65,22 @@ int	get_row_count(char *fname)
 	fd = open(fname, O_RDONLY);
 	if (fd < 0)
 		return (1);
-	str = get_next_line(fd);
-	while (str)
+	str = trim(get_next_line(fd));
+	map->col_count = ft_strlen(str);
+	while (str && ++i)
 	{
+		if (ft_strlen(str) != map->col_count || illegal_char(str))
+			return (1);
+		map->c_count += char_count(str, 'C');
+		map->p_count += char_count(str, 'P');
+		map->e_count += char_count(str, 'E');
+		map->x_count += char_count(str, 'X');
 		free(str);
-		str = get_next_line(fd);
-		i++;
+		str = trim(get_next_line(fd));
 	}
-	close(fd);
-	return (i);
-}
-
-char	*trim(char *arr)
-{
-	if (arr[ft_strlen(arr) - 1] == '\n')
-		arr[ft_strlen(arr) - 1] = 0;
-	return (arr);
+	if (!map->c_count || map->p_count != 1 || map->e_count != 1)
+		return (close (fd), 1);
+	return (map->row_count = i, close (fd), 0);
 }
 
 int	read_map(char *fname, t_map *map)
@@ -53,11 +89,10 @@ int	read_map(char *fname, t_map *map)
 	int		i;
 	char	*temp;
 
+	if (set_counts(map, fname))
+		return (1);
 	fd = open(fname, O_RDONLY);
 	if (fd < 0)
-		return (1);
-	map->row_count = get_row_count(fname);
-	if (!map->row_count)
 		return (1);
 	map->map = malloc((map->row_count + 1) * sizeof(char *));
 	if (!map->map)
@@ -85,54 +120,27 @@ void	print_map(t_map *map)
 	}
 }
 
-int	check_escape(t_map *map)
+char	**dup_map(int rc, char **map)
 {
-	return (0);
-}
+	char	**arr;
+	int		i;
 
-int	check_rectangle(t_map *map)
-{
-	int	i;
-
+	arr = malloc(sizeof(char *) * (rc + 1));
 	i = 0;
-	while (map->map[i])
-		if (ft_strlen(map->map[i++]) != map->col_count)
-			return (1);
-	return (0);
-}
-
-int	is_legal_char(char ch)
-{
-	if (ch == '1' || ch == '0' || ch == 'P' || ch == 'E' || ch == 'C')
-		return (0);
-	return (1);
-}
-
-int	check_illegal_char(t_map *map)
-{
-	int	i;
-	int	j;
-	int	flag[2];
-
-	ft_memset(flag, 0, (sizeof(int) * 2));
-	i = 0;
-	while (map->map[i])
+	while (map[i])
 	{
-		j = 0;
-		while (map->map[i][j])
-		{
-			if (map->map[i][j] == 'P')
-				flag[0]++;
-			if (map->map[i][j] == 'E')
-				flag[1]++;
-			if (is_legal_char(map->map[i][j]))
-				return (1);
-			j++;
-		}
+		arr[i] = ft_strdup(map[i]);
 		i++;
 	}
-	if (flag[0] != 1 || flag[1] != 1)
-		return (1);
+	arr[i] = NULL;
+	return (arr);
+}
+
+int	check_escape(t_map *map)
+{
+	// char	**map_copy;
+
+	// map_copy = dup_map(map->row_count, map->map);
 	return (0);
 }
 
@@ -141,7 +149,6 @@ int	check_borders(t_map *map)
 	int	i;
 
 	i = 0;
-	map->col_count = ft_strlen(map->map[0]);
 	while (i < map->col_count)
 		if (map->map[0][i++] != '1')
 			return (1);
@@ -162,32 +169,32 @@ int	check_borders(t_map *map)
 
 int	validate_map(t_map *map)
 {
-	if (check_borders(map) || check_illegal_char(map)
-		|| check_rectangle(map) || check_escape(map))
+	int	flag;
+
+	if (check_borders(map) || check_escape(map))
 		return (1);
 	return (0);
 }
 
-int	destructor(t_map *map)
+int	destructor(char **map)
 {
 	int	i;
 
 	i = 0;
-	while (map->map[i])
-		free(map->map[i++]);
-	free(map->map);
+	while (map[i])
+		free(map[i++]);
+	free(map);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_map	map;
 
+	ft_bzero(&map, sizeof(t_map));
 	if (argc != 2)
 		return (ft_printf("Invalid number of arguments\n"), 1);
-	if (check_extension(argv[1]))
-		return (ft_printf("Invalid file extension\n"), 1);
-	if (read_map(argv[1], &map))
-		return (ft_printf("Failed to read the file or empty file\n", 1));
+	if (check_extension(argv[1]) || read_map(argv[1], &map))
+		return (ft_printf("Invalid map or file\n", 1));
 	if (validate_map(&map))
 		return (destructor(&map), ft_printf("Invalid map\n"), 1);
 	print_map(&map);
